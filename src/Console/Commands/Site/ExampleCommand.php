5847cca4b5f49da2e9b6697882303375ac5f2ac4
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2021-04-19 14:11:12 +0300

    site:example README.md

--- src/Console/Commands/Site/ExampleCommand.php
+++ src/Console/Commands/Site/ExampleCommand.php
@@ -7,6 +7,7 @@ namespace Flipsite\Console\Commands\Site;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -27,8 +28,14 @@ final class ExampleCommand extends Command
         $io         = new SymfonyStyle($input, $output);
         $filesystem = new Filesystem();
 
-        $siteFilename  = getenv('SITE_DIR').'/site.yaml';
-        $themeFilename = getenv('SITE_DIR').'/theme.yaml';
+        $question = new Question('Please enter the name of your site: ', );
+
+        $helper = $this->getHelper('question');
+        $name   = $helper->ask($input, $output, $question);
+
+        $siteFilename   = getenv('SITE_DIR').'/site.yaml';
+        $themeFilename  = getenv('SITE_DIR').'/theme.yaml';
+        $readmeFilename = getenv('SITE_DIR').'/README.md';
         if (file_exists($siteFilename) || file_exists($themeFilename)) {
             $io->error('A site already exists.');
         } else {
@@ -39,7 +46,11 @@ final class ExampleCommand extends Command
                 $filesystem->mkdir($assetDir);
             }
             $filesystem->copy(__DIR__.'/flipsite.svg', $assetDir.'/flipsite.svg');
-            $io->success('Example site created');
+            $readme = file_get_contents(__DIR__.'/README.md');
+            $readme = str_replace('{{name}}', $name, $readme);
+            file_put_contents($readmeFilename, $readme);
+
+            $io->success('Example site created for '.$name);
         }
         return 0;
     }
---
+++ src/Console/Commands/Site/README.md
@@ -0,0 +1,19 @@
+# {{name}}
+
+### Install
+
+Install Flipsite and {{name}} dependencies with [Composer](https://getcomposer.org/).
+```
+cd path/to/{{name}}
+composer install
+```
+
+### Run
+Start the development server. Usually [127.0.0.1:8000](http://127.0.0.1:8000/) otherwise check console log.
+```
+./vendor/bin/flipsite server:run
+```
+
+### Documentation
+
+Full documentation at [docs.flipsite.io](https://docs.flipsite.io).
