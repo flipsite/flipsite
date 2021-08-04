8717c28f6135c031e2e50f1af7001fd1789f5072
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2021-08-04 15:01:12 +0300

    Markdown component container support

--- src/Components/Md.php
+++ src/Components/Md.php
@@ -9,6 +9,7 @@ final class Md extends AbstractComponent
     use Traits\MarkdownTrait;
     use Traits\EnviromentTrait;
     protected string $type = 'div';
+    private Element $container;
 
     public function with(ComponentData $data) : void
     {
@@ -22,11 +23,21 @@ final class Md extends AbstractComponent
             $markdown = $value;
         }
         $this->content = $this->getMarkdown($markdown ?? '', $style ?? null);
+        $containerStyle = $data->getStyle('container');
+        if ($containerStyle) {
+            $this->container = new Element($containerStyle['type']??'div');
+            unset($containerStyle['type']);
+            $this->container->addStyle($containerStyle);
+            ;
+        }
     }
 
     public function render(int $indentation = 2, int $level = 0, bool $oneline = false) : string
     {
         $i    = str_repeat(' ', $indentation * $level);
+        if ($this->container) {
+            $i++;
+        }
         $html = $this->content;
         $html = str_replace("\n", ' ', $html);
         $tags = explode('-#-#-#-', str_replace('> <', '>-#-#-#-<', $html));
@@ -34,6 +45,11 @@ final class Md extends AbstractComponent
         // foreach ($tags as $tag) {
         //     $html .= $i.wordwrap($tag, 80, "\n".$i)."\n";
         // }
-        return $html;
+        if (!$this->container) {
+            return $html;
+        } else {
+            $this->container->setContent($html);
+            return $this->container->render($indentation, $level, $oneline);
+        }
     }
 }
