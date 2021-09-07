635018b4cdcd6768aa7c4a7cd75c7a3a2db1fa55
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2021-09-07 11:13:35 +0300

    Video component

--- src/Components/youtube.js
+++ src/Components/youtube.js
@@ -1,6 +1,8 @@
 function youtube(el) {
   var icon = el.querySelector('svg');
   if (icon) icon.remove();
+  var picture = el.querySelector('picture');
+  if (picture) picture.remove();
   el.classList.remove('hover:cursor-pointer');
   el.removeAttribute('onclick')
   var iframe = el.querySelector('iframe');
