07bf23a1eb85599079dd91c3055c939af173fe84
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2021-09-27 12:47:24 +0300

    SVG render nicer

--- src/App/Middleware/SvgMiddleware.php
+++ src/App/Middleware/SvgMiddleware.php
@@ -42,23 +42,25 @@ class SvgMiddleware implements MiddlewareInterface
                             'def'     => $svgData->getDef(),
                         ];
                         ++$id;
-                        $defs .= '        <g id="'.$svgs[$src]['id'].'">';
-                        $defs .= $svgs[$src]['def'];
-                        $defs .= "</g>\n";
+                        $def  = '<g id="'.$svgs[$src]['id'].'">';
+                        $def .= $svgs[$src]['def'];
+                        $def .= "</g>\n";
+                        $def = preg_replace('!\s+!', ' ', $def);
+                        $def = str_replace("\n", ' ', $def);
+                        $defs.= '        '.$def."\n";
                     }
                 }
-
                 $with = $matches[0][$i];
                 $with = str_replace(' src="'.$src.'"', '', $with);
                 $with = str_replace('xlink:href=""', 'xlink:href="#'.$svgs[$src]['id'].'"', $with);
                 $with = str_replace('viewBox=""', 'viewBox="'.$svgs[$src]['viewbox'].'"', $with);
                 $html = str_replace($matches[0][$i], $with, $html);
             }
-            $svg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: none;">'."\n";
-            $svg .= '      <defs>'."\n";
-            $svg .= str_replace('></path>', '/>', $defs);
-            $svg .= "      </defs>\n";
-            $svg .= '    </svg>';
+            $svg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: none;">';
+            $svg.= "\n      <defs>\n";
+            $defs = str_replace('></path>', '/>', $defs);
+            $svg.=           $defs;
+            $svg .= "      </defs>\n    </svg>";
             $html = str_replace('<svg></svg>', $svg, $html);
         }
         $streamFactory = new StreamFactory();
