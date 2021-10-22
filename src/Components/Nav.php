e8542834ca676e3df6533da3e1eeb813b15ee616
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2021-10-22 11:01:52 +0300

    Active check fix when page links

--- src/Components/Nav.php
+++ src/Components/Nav.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 namespace Flipsite\Components;
 
 use Flipsite\Utils\ArrayHelper;
@@ -67,12 +66,13 @@ final class Nav extends AbstractComponent
         foreach ($items as &$item) {
             // If URL is an array, it's a localized external URL that cannot be active
             if (isset($item['url']) && is_string($item['url'])) {
+                $url              = explode('#', $item['url'])[0];
                 $item['isActive'] = $item['isActive'] ?? false;
-                $item['exact']    = $item['exact']    ?? false;
-                if ($item['exact'] && $item['url'] === $active) {
+                $item['exact']    = $item['exact'] ?? false;
+                if ($item['exact'] && $url === $active) {
                     $item['isActive'] = true;
                 } elseif (!$item['exact']) {
-                    $item['isActive'] = $item['url'] === mb_substr($active, 0, mb_strlen($item['url']));
+                    $item['isActive'] = $url === mb_substr($active, 0, mb_strlen($url));
                 }
             }
             unset($item['exact']);
