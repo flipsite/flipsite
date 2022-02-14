ff659292ff31c525b1f29a0692e0f80fdbb77c47
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2022-02-14 15:41:31 +0200

    Langauges fixes

--- src/Components/Languages.php
+++ src/Components/Languages.php
@@ -8,9 +8,10 @@ class Languages extends Nav
     public function normalize(string|int|bool|array $data) : array
     {
         $active = $this->path->getLanguage();
-        if (is_string($data)) {
-            $languages = explode(',', $data);
-            $data      = ['items'=>[]];
+        if (is_string($data) || is_string($data['items'])) {
+            $items     = is_string($data) ? $data : $data['items'];
+            $languages = explode(',', str_replace(' ', '', $items));
+            $data      = ['items'=>[], 'options'=>$data['options'] ?? []];
             foreach ($languages as $lang) {
                 $data['items'][] = [
                     'url'      => $lang,
@@ -25,7 +26,7 @@ class Languages extends Nav
     public function build(array $data, array $style, string $appearance) : void
     {
         // TODO add aria stuff for accessibility
-        if (in_array('full', $data['flags'])) {
+        if (in_array('full', $data['flags']) || ($data['options']['full'] ?? false)) {
             foreach ($data['items'] as &$item) {
                 $item['text'] = $this->getString($item['url']);
             }
@@ -36,7 +37,7 @@ class Languages extends Nav
     private function getString(string $languageCode) : string
     {
         switch ($languageCode) {
-            case 'en': return 'English';
+            case 'en': return 'In English';
             case 'fi': return 'Suomeksi';
             case 'sv': return 'Svenska';
             default: return $languageCode;
