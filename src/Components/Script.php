882f9c1ccaeea23b3bcae1fcd2016e08b18a404b
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2022-02-16 13:38:05 +0200

    Script component

---
+++ src/Components/Script.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+namespace Flipsite\Components;
+
+final class Script extends AbstractComponent
+{
+    protected string $tag   = 'script';
+    protected bool $oneline = true;
+
+    public function normalize(string|int|bool|array $data) : array
+    {
+        if (!is_array($data)) {
+            return ['src' => $data];
+        }
+        return $data;
+    }
+
+    public function build(array $data, array $style, string $appearance) : void
+    {
+        $this->setAttribute('src',$data['src']);
+    }
+}
