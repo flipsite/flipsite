a59f699c480cdccce74eb1242cd43d981ba1738b
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2022-02-17 12:38:43 +0200

    AspectRatio rule

---
+++ src/Style/Rules/RuleAspectRatio.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+namespace Flipsite\Style\Rules;
+
+final class RuleAspectRatio extends AbstractRuleSpacing
+{
+    /**
+     * @param array<string> $args
+     */
+    protected function process(array $args) : void
+    {
+        $this->setDeclaration('aspect-ratio', implode('/', $args));
+        // $properties = ['left', 'right', 'top', 'bottom'];
+        // if ('x' === $args[0]) {
+        //     array_shift($args);
+        //     $properties = ['left', 'right'];
+        // } elseif ('y' === $args[0]) {
+        //     array_shift($args);
+        //     $properties = ['top', 'bottom'];
+        // }
+        // $value = $this->getConfig('inset', $args[0]);
+        // $value ??= $this->getConfig('spacing', $args[0]);
+        // $value ??= $this->checkCallbacks('size', $args);
+        // foreach ($properties as $property) {
+        //     $this->setDeclaration($property, $value);
+        // }
+    }
+}
