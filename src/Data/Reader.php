b136d9961fc9a3300209e5584eb3160ce74ffe63
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2022-02-21 14:46:23 +0200

    Hide sections with no data

--- src/Data/Reader.php
+++ src/Data/Reader.php
@@ -283,6 +283,18 @@ final class Reader
 
     private function hideSection(array $section, string $page, Language $language) : bool
     {
+        $_     = 0;
+        $total = 0;
+        foreach ($section as $attr => $val) {
+            $total++;
+            if (str_starts_with($attr, '_')) {
+                $_++;
+            }
+        }
+        if ($_ === $total) {
+            return true;
+        }
+
         if ($section['hidden'] ?? false) {
             return true;
         }
