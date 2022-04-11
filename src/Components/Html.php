8692301039622afa34e6d37b05cc34562183b795
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2022-04-11 11:21:34 +0300

    HTML component

---
+++ src/Components/Html.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+namespace Flipsite\Components;
+
+use Flipsite\Utils\ArrayHelper;
+use Flipsite\Utils\StyleAppearanceHelper;
+
+final class Html extends AbstractComponent
+{
+    use Traits\EnviromentTrait;
+
+    public function build(array $data, array $style, string $appearance) : void
+    {
+        if (mb_strpos($data['value'], '.html')) {
+            $filename = $this->enviroment->getSiteDir().'/'.$data['value'];
+            if (file_exists($filename)) {
+                $html = file_get_contents($filename);
+            }
+        } else {
+            $html = $data['value'];
+        }
+        $this->content = $html;
+    }
+
+    public function render(int $indentation = 2, int $level = 0, bool $oneline = false) : string
+    {
+        $i   = str_repeat(' ', $indentation * ($level + 1));
+        return $i.str_replace("\n","\n".$i,$this->content);
+    }
+}
