64e9d86b80e394849af4f54bbd76e15589ba6a1f
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2022-05-24 14:22:30 +0300

    New web server

--- src/Console/app.php
+++ src/Console/app.php
@@ -7,11 +7,7 @@ use Symfony\Component\Console\Application;
 
 require_once getenv('VENDOR_DIR').'/autoload.php';
 
-$app = new Application('Flipsite', 'v0.8');
-if (class_exists('Symfony\Bundle\WebServerBundle\Command\ServerRunCommand')) {
-    $app->add(new Symfony\Bundle\WebServerBundle\Command\ServerRunCommand(getenv('VENDOR_DIR').'/flipsite/flipsite', 'dev'));
-}
-
+$app      = new Application('Flipsite', 'v0.9');
 $commands = Commands::getCommands(getenv('VENDOR_DIR').'/flipsite/flipsite/src/Console/Commands');
 foreach ($commands as $class) {
     $app->add(new $class());
