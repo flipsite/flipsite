07e5ec877c16e590e4cb21aad8125fada888670a
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2022-09-28 10:54:58 +0300

    RuleColumns

---
+++ src/Style/Rules/RuleColumns.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+namespace Flipsite\Style\Rules;
+
+final class RuleColumns extends AbstractRule
+{
+    /**
+     * @param array<string> $args
+     */
+    protected function process(array $args) : void
+    {
+        $value = intval($args[0]);
+        $this->setDeclaration('columns', $value);
+    }
+}
