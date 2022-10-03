8be5d8b19aed7c5df4d1ad507d54cc68d965039f
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2022-10-03 16:17:32 +0300

    onclick fix

--- src/Components/AbstractGroup.php
+++ src/Components/AbstractGroup.php
@@ -20,6 +20,14 @@ abstract class AbstractGroup extends AbstractComponent
             $style      = ArrayHelper::merge($nthStyle, $style);
         }
 
+        if (isset($data['onclick'])) {
+            $this->setAttribute('onclick', $data['onclick']);
+            if (strpos($data['onclick'], 'javascript:toggle') === 0) {
+                $this->builder->dispatch(new Event('global-script', 'toggle', file_get_contents(__DIR__.'/../../js/toggle.min.js')));
+            }
+            unset($data['onclick']);
+        }
+
         $that           = $this;
         $wrapperStyle   = $style['wrapper'] ?? false;
         $containerStyle = $style['container'] ?? false;
