56c6e05a9521c45db15992bfaa4eea6e6fb61d04
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2022-11-29 16:58:29 +0200

    Map fix

--- src/Components/Map.php
+++ src/Components/Map.php
@@ -18,9 +18,9 @@ final class Map extends AbstractComponent
         foreach ($data as $key => $val) {
             $this->setAttribute($key, $val);
         }
-        $src = 'https://maps.google.com/maps?q='.$data['name'].','.urlencode($data['address']).'&t=&z=15&ie=UTF8&iwloc=&output=embed';
+        $src = $data['src'] ?? 'https://maps.google.com/maps?q='.$data['name'].','.urlencode($data['address']).'&t=&z=15&ie=UTF8&iwloc=&output=embed';
         $this->setAttribute('data-src-onenter', $src);
-        $this->setAttribute('title', $data['title'] ?? $data['name'].', '.$data['address']);
+        $this->setAttribute('title', $data['title'] ?? $data['name'] ?? 'map'.','.($data['address'] ?? ''));
         $this->builder->dispatch(new Event('ready-script', 'iframe-onenter', file_get_contents(__DIR__.'/../../js/ready.iframe-onenter.min.js')));
     }
 }
