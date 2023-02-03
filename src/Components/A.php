a676e5f59d977b23b3f48267a55193d36da8ea4c
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2023-02-03 07:55:32 +0200

    broken

--- src/Components/A.php
+++ src/Components/A.php
@@ -9,73 +9,57 @@ final class A extends AbstractGroup
 
     protected string $tag = 'a';
 
-    public function build(array $data, array $style, string $appearance) : void
-    {
-        // Old fallback
-        if (isset($data['url'])) {
-            $data['href'] = $data['url'];
-            unset($data['url']);
-        }
-        $urlData  = $this->expand($data);
-        $external = false;
-        $href     = isset($urlData['href']) ? $this->url((string)$urlData['href'], $external) : '';
-        if ($href) {
-            $this->setAttribute('href', $href);
-            if ($external) {
-                $this->setAttribute('target', '_blank');
-                $this->setAttribute('rel', 'noopener noreferrer');
-            }
-        } else {
-            $this->tag = 'div';
-        }
-        parent::build($urlData, $style, $appearance);
-    }
-
     public function normalize(string|int|bool|array $data) : array
     {
-        if (!is_array($data)) {
-            return ['text' => $data, 'href' => '#'];
+        // TODO until nav is improved
+        if (isset($data['url'])) {
+            $data['action'] = 'page';
+            $data['target'] = $data['url'];
+            unset($data['url']);
         }
-        return $data;
-    }
 
-    private function expand(array $data) : array
-    {
-        $expanded = [];
-        foreach ($data as $key => $val) {
-            switch ($key) {
-                case 'tel':
-                    $expanded['href'] = 'tel:'.$val;
-                    $phoneUtil       = \libphonenumber\PhoneNumberUtil::getInstance();
-                    try {
-                        $numberProto = $phoneUtil->parse($val, '');
-                    } catch (\libphonenumber\NumberParseException $e) {
+        $href = null;
+        $replace = [];
+        $data['attr'] ??= [];
+        switch ($data['action'] ?? []) {
+            case 'page':
+            case 'url':
+            case 'url-blank':
+                $external = false;
+                $href = $this->url($data['target'], $external);
+                if ($external) {
+                    $data['attr']['rel'] = 'noopener noreferrer';
+                    if ('url-blank' === $data['action']) {
+                        $data['attr']['target'] = '_blank';
                     }
-                    $format = \libphonenumber\PhoneNumberFormat::NATIONAL;
-                    // if (in_array('international', [])) {
-                    //     $format = \libphonenumber\PhoneNumberFormat::INTERNATIONAL;
-                    // } elseif (in_array('e164', [])) {
-                    //     $format = \libphonenumber\PhoneNumberFormat::E164;
-                    // }
-                    $number = $phoneUtil->format($numberProto, $format);
-                    if (isset($data['text'])) {
-                        $expanded['text'] = sprintf($data['text'], $number);
-                    } else {
-                        $expanded['text'] = $number;
-                    }
-                    break;
-                case 'mailto':
-                    $expanded['href']  = 'mailto:'.$val;
-                    if (isset($data['text'])) {
-                        $expanded['text'] = sprintf($data['text'], $val);
-                    } else {
-                        $expanded['text'] = $val;
-                    }
-                    break;
-                default:
-                    $expanded[$key] = $val;
-            }
+                }
+                break;
+            case 'tel':
+                $tel = '+'.trim($data['target'], '+');
+                $phoneUtil  = \libphonenumber\PhoneNumberUtil::getInstance();
+                try {
+                    $numberProto = $phoneUtil->parse($tel, '');
+                } catch (\libphonenumber\NumberParseException $e) {
+                }
+                $href = 'tel:'.$tel;
+                $replace = [
+                    '{{tel}}' => $phoneUtil->format($numberProto, \libphonenumber\PhoneNumberFormat::NATIONAL),
+                    '{{tel.int}}' => $phoneUtil->format($numberProto, \libphonenumber\PhoneNumberFormat::INTERNATIONAL),
+                    '{{tel.e164}}' => $phoneUtil->format($numberProto, \libphonenumber\PhoneNumberFormat::E164),
+                ];
+                break;
+            case 'mailto':
+                $href = 'mailto:'.$data['target'];
+                $replace = [
+                    '{{email}}' => $data['target']
+                ];
+                break;
+            default:
+                $href = '#';
         }
-        return $expanded;
+        $data['_attr']['href'] = $href;
+        unset($data['action']);
+        unset($data['traget']);
+        return $data;
     }
 }
--- src/Components/Nav.php
+++ src/Components/Nav.php
@@ -26,55 +26,55 @@ class Nav extends AbstractGroup
             $data['items'] = $this->getFromSlugs($data['items']);
         }
 
-        if (isset($data['repeat'],$data['item'])) {
-            // TODO maybe import file content here
-            $data['items'] = $this->expandRepeat($data['repeat'], $data['item']);
-            unset($data['repeat'], $data['item']);
-        }
+        // if (isset($data['repeat'],$data['item'])) {
+        //     // TODO maybe import file content here
+        //     $data['items'] = $this->expandRepeat($data['repeat'], $data['item']);
+        //     unset($data['repeat'], $data['item']);
+        // }
 
-        if (ArrayHelper::isAssociative($data) && !isset($data['items'])) {
-            $style = $data['style'] ?? [];
-            unset($data['style']);
-            $data = ['items' => $data, 'style'=>$style];
-        }
+        // if (ArrayHelper::isAssociative($data) && !isset($data['items'])) {
+        //     $style = $data['_style'] ?? [];
+        //     unset($data['_style']);
+        //     $data = ['items' => $data, '_style'=>$style];
+        // }
 
-        if (ArrayHelper::isAssociative($data['items'])) {
-            $items = [];
-            foreach ($data['items'] as $url => $value) {
-                $args = explode('|', $url);
-                $url  = array_shift($args);
-                if (is_string($value)) {
-                    $item = [
-                        'url'  => $url,
-                        'text' => $value,
-                    ];
-                } else {
-                    if (!isset($value['text'])) {
-                        $item = ['text' => $value];
-                    } else {
-                        $item = $value;
-                    }
-                    if (!isset($item['url'])) {
-                        $item['url'] = $url;
-                    }
-                }
-                // Inline options, e.g. |exact
-                foreach ($args as $attr) {
-                    $item[$attr] = true;
-                }
-                $items[]       = $item;
-                $data['items'] = $items;
-            }
-        }
+        // if (ArrayHelper::isAssociative($data['items'])) {
+        //     $items = [];
+        //     foreach ($data['items'] as $url => $value) {
+        //         $args = explode('|', $url);
+        //         $url  = array_shift($args);
+        //         if (is_string($value)) {
+        //             $item = [
+        //                 'url'  => $url,
+        //                 'text' => $value,
+        //             ];
+        //         } else {
+        //             if (!isset($value['text'])) {
+        //                 $item = ['text' => $value];
+        //             } else {
+        //                 $item = $value;
+        //             }
+        //             if (!isset($item['url'])) {
+        //                 $item['url'] = $url;
+        //             }
+        //         }
+        //         // Inline options, e.g. |exact
+        //         foreach ($args as $attr) {
+        //             $item[$attr] = true;
+        //         }
+        //         $items[]       = $item;
+        //         $data['items'] = $items;
+        //     }
+        // }
 
-        if ($data['options']['exact'] ?? false) {
-            $data['items'][0]['exact'] = true;
-        }
+        // if ($data['options']['exact'] ?? false) {
+        //     $data['items'][0]['exact'] = true;
+        // }
 
-        if ($data['options']['remember'] ?? false) {
-            $this->setAttribute('data-remember', true);
-            $this->builder->dispatch(new Event('ready-script', 'toggle', file_get_contents(__DIR__.'/../../js/ready.remember.min.js')));
-        }
+        // if ($data['options']['remember'] ?? false) {
+        //     $this->setAttribute('data-remember', true);
+        //     $this->builder->dispatch(new Event('ready-script', 'toggle', file_get_contents(__DIR__.'/../../js/ready.remember.min.js')));
+        // }
 
         if (isset($data['options'])) {
             $offset        = $data['options']['offset'] ?? 0;
@@ -82,58 +82,58 @@ class Nav extends AbstractGroup
             $data['items'] = array_slice($data['items'], $offset, $length);
         }
 
-        if (isset($data['prepend'])) {
-            $data['items'] = array_merge($data['prepend'], $data['items']);
-            unset($data['prepend']);
-        }
-        if (isset($data['append'])) {
-            $data['items'] = array_merge($data['items'], $data['append']);
-            unset($data['append']);
-        }
+        // if (isset($data['prepend'])) {
+        //     $data['items'] = array_merge($data['prepend'], $data['items']);
+        //     unset($data['prepend']);
+        // }
+        // if (isset($data['append'])) {
+        //     $data['items'] = array_merge($data['items'], $data['append']);
+        //     unset($data['append']);
+        // }
 
         return $data;
     }
 
     public function build(array $data, array $style, string $appearance) : void
     {
-        $setState = in_array('setState', $data['flags'] ?? []);
-        if (!$setState) {
+        // $setState = in_array('setState', $data['flags'] ?? []);
+        // if (!$setState) {
             $items = $this->addActive($data['items'], $this->path->getPage());
-        } else {
-            if (!isset($data['flags'][1])) {
-                throw new \Exception('No setstate target');
-            }
-            $this->builder->dispatch(new Event('global-script', 'setState', file_get_contents(__DIR__.'/../../js/setState.min.js')));
-            $items = [];
-            foreach ($data['items'] as $item) {
-                $url = $item['url'];
-                unset($item['url']);
-                $item['onclick'] = "javascript:setState('".$data['flags'][1]."','".$url."')";
-                $items[]         = $item;
-            }
-            $this->setAttribute('data-not-active', implode(' ', $style['notActive'] ?? []));
-            $this->setAttribute('data-active', implode(' ', $style['active'] ?? []));
-        }
+        // } else {
+        //     if (!isset($data['flags'][1])) {
+        //         throw new \Exception('No setstate target');
+        //     }
+        //     $this->builder->dispatch(new Event('global-script', 'setState', file_get_contents(__DIR__.'/../../js/setState.min.js')));
+        //     $items = [];
+        //     foreach ($data['items'] as $item) {
+        //         $url = $item['url'];
+        //         unset($item['url']);
+        //         $item['onclick'] = "javascript:setState('".$data['flags'][1]."','".$url."')";
+        //         $items[]         = $item;
+        //     }
+        //     $this->setAttribute('data-not-active', implode(' ', $style['notActive'] ?? []));
+        //     $this->setAttribute('data-active', implode(' ', $style['active'] ?? []));
+        // }
         unset($data['items']);
 
         $last = count($items) - 1;
 
         $notActiveStyle = $style['notActive'] ?? [];
         foreach ($items as $i => $item) {
-            $item['style'] = ArrayHelper::merge($style['items'] ?? [], $item['style'] ?? []);
+            $item['_style'] = ArrayHelper::merge($style['items'] ?? [], $item['_style'] ?? []);
             if ($i === 0 && isset($style['first'])) {
-                $item['style'] = ArrayHelper::merge($item['style'], $style['first']);
+                $item['_style'] = ArrayHelper::merge($item['_style'], $style['first']);
             } elseif ($i === $last && isset($style['last'])) {
-                $item['style'] = ArrayHelper::merge($item['style'], $style['last']);
+                $item['_style'] = ArrayHelper::merge($item['_style'], $style['last']);
             }
             if (isset($item['active']) && $item['active'] && isset($style['active'])) {
                 unset($item['active']);
-                if (!isset($item['style'])) {
-                    $item['style'] = [];
+                if (!isset($item['_style'])) {
+                    $item['_style'] = [];
                 }
-                $item['style'] = ArrayHelper::merge($item['style'], $style['active']);
+                $item['_style'] = ArrayHelper::merge($item['_style'], $style['active']);
             } elseif (!($item['active'] ?? false) && $notActiveStyle) {
-                $item['style'] = ArrayHelper::merge($item['style'], $notActiveStyle);
+                $item['_style'] = ArrayHelper::merge($item['_style'], $notActiveStyle);
             }
             $data['a:'.$i] = $item;
         }
@@ -149,15 +149,15 @@ class Nav extends AbstractGroup
         $activeParts = explode('/', $active);
         foreach ($items as &$item) {
             // If URL is an array, it's a localized external URL that cannot be active
-            if (isset($item['url']) && is_string($item['url'])) {
-                $url              = explode('#', $item['url'])[0];
+            if (isset($item['target']) && is_string($item['target'])) {
+                $slug              = explode('#', $item['target'])[0];
                 $item['active']   = $item['active'] ?? false;
                 $item['exact']    = $item['exact'] ?? false;
-                if ($item['exact'] && $url === $active) {
+                if ($item['exact'] && $slug === $active) {
                     $item['active'] = true;
                 } elseif (!$item['exact']) {
-                    $urlParts       = explode('/', $url);
-                    $item['active'] = $this->compare($urlParts, $activeParts);
+                    $slugParts       = explode('/', $slug);
+                    $item['active'] = $this->compare($slugParts, $activeParts);
                 }
             }
             unset($item['exact']);
@@ -165,15 +165,15 @@ class Nav extends AbstractGroup
         return $items;
     }
 
-    private function compare(array $url, array $active): bool
+    private function compare(array $slug, array $active): bool
     {
         $same = 0;
-        foreach ($url as $i => $a) {
+        foreach ($slug as $i => $a) {
             if ($a === ($active[$i] ?? '')) {
                 $same++;
             }
         }
-        return $same >= count($url);
+        return $same >= count($slug);
     }
 
     private function getFromSlugs(string $page) : ?array
@@ -207,7 +207,8 @@ class Nav extends AbstractGroup
         $items = [];
         foreach ($pages as $page) {
             $item            = [
-                'url'  => $page,
+                'action' => 'page',
+                'target'  => $page,
                 'text' => $this->reader->getPageName((string)$page, $this->path->getLanguage())
             ];
             if ($firstExact) {
