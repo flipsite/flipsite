f1513c2f28160762be0712d92142122a8d515346
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2023-07-11 23:52:31 +0300

    Exception fix

--- src/Environment.php
+++ src/Environment.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 namespace Flipsite;
 
 use Flipsite\Utils\Plugins;
@@ -14,29 +15,29 @@ final class Environment extends AbstractEnvironment
         $this->live = 'live' === getenv('APP_ENV');
 
         if (false === getenv('IMG_DIR')) {
-            throw new EnvironmentException('IMG_DIR not set');
+            throw new \Exception('IMG_DIR not set');
         }
         $this->imgDir = getenv('IMG_DIR');
 
         if (false === getenv('VIDEO_DIR')) {
-            throw new EnvironmentException('VIDEO_DIR not set');
+            throw new \Exception('VIDEO_DIR not set');
         }
         $this->videoDir = getenv('VIDEO_DIR');
 
         if (false === getenv('SITE_DIR')) {
-            throw new EnvironmentException('SITE_DIR not set');
+            throw new \Exception('SITE_DIR not set');
         }
         $this->siteDir = getenv('SITE_DIR');
 
         $this->vendorDir = getenv('VENDOR_DIR');
 
         if (false === getenv('APP_BASEPATH')) {
-            throw new EnvironmentException('APP_BASEPATH not set');
+            throw new \Exception('APP_BASEPATH not set');
         }
         $this->basePath = getenv('APP_BASEPATH');
 
         if (false === getenv('APP_SERVER')) {
-            throw new EnvironmentException('APP_SERVER not set');
+            throw new \Exception('APP_SERVER not set');
         }
         $this->server = trim(getenv('APP_SERVER'), '/');
 
