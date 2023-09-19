27719035268018f2998570f5f97a7e1b359df092
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2023-09-19 22:43:09 +0300

    Compiler

---
+++ src/Compiler/AssetParser.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+namespace Flipsite\Compiler;
+
+
+class AssetParser {
+    public static function parse(string $html): array
+    {
+        $assets = [];
+
+        $html = str_replace("\n", '', $html);
+        $html = preg_replace('/\s+/', ' ', $html);
+        $html = str_replace('> <', '><', $html);
+        $html = str_replace('> ', '>', $html);
+        $html = str_replace(' <', '<', $html);
+
+        libxml_use_internal_errors(true);
+        $doc = new \DOMDocument();
+        $doc->loadHTML($html);
+
+        $imgTags = $doc->getElementsByTagName('img');
+        foreach ($imgTags as $tag) {
+            $src = $tag->getAttribute('src');
+            if (str_starts_with($src, 'data:')) {
+                continue;
+            }
+            $assets[] = $src;
+            $tmp      = explode(', ', $tag->getAttribute('srcset'));
+            foreach ($tmp as $t) {
+                $tmp2     = explode(' ', $t);
+                $assets[] = $tmp2[0];
+            }
+        }
+
+        $videoTags = $doc->getElementsByTagName('video');
+        foreach ($videoTags as $tag) {
+            $poster = $tag->getAttribute('poster');
+            if ($poster) {
+                $assets[] = $poster;
+            }
+        }
+
+        $sourceTags = $doc->getElementsByTagName('source');
+        foreach ($sourceTags as $tag) {
+            $src = $tag->getAttribute('src');
+            if ($src) {
+                $assets[] = $src;
+            }
+        }
+
+        $metaTags = $doc->getElementsByTagName('meta');
+        foreach ($metaTags as $tag) {
+            if ('og:image' === $tag->getAttribute('property')) {
+                $url      = $tag->getAttribute('content');
+                $tmp      = explode('/img/', $url);
+                $assets[] = '/img/'.$tmp[1];
+            }
+        }
+        $linkTags = $doc->getElementsByTagName('link');
+        foreach ($linkTags as $tag) {
+            if ('icon' === $tag->getAttribute('rel')) {
+                $url      = $tag->getAttribute('href');
+            } elseif ('apple-touch-icon' === $tag->getAttribute('rel')) {
+                $url      = $tag->getAttribute('href');
+            }
+            if (!str_starts_with($url, 'data:')) {
+                $assets[] = $url;
+            }
+        }
+
+        $matches = [];
+        preg_match_all('/url\(\/img\/(.*?)\)/', $html, $matches);
+        if (isset($matches[1])) {
+            foreach ($matches[1] as $asset) {
+                $assets[] = '/img/'.$asset;
+            }
+        }
+
+        return array_values(array_unique($assets));
+    }
+}
---
+++ src/Compiler/Compiler.php
@@ -0,0 +1,178 @@
+<?php
+
+declare(strict_types=1);
+namespace Flipsite\Compiler;
+
+use Flipsite\AbstractEnvironment;
+use Flipsite\Data\Reader;
+use Symfony\Component\Filesystem\Filesystem;
+
+class Compiler
+{
+    private string $targetDir;
+
+    public function __construct(private AbstractEnvironment $environment, string $targetDir)
+    {    
+        $this->targetDir = realpath($targetDir);
+
+        putenv('SITE_DIR='.$this->environment->getSiteDir());
+        putenv('VENDOR_DIR='.$this->environment->getVendorDir());
+        putenv('IMG_DIR='.$this->environment->getImgDir());
+        putenv('VIDEO_DIR='.$this->environment->getVideoDir());
+        putenv('TRAILING_SLASH=1');
+
+        // Create target dir if it does not already exist
+        if (!is_dir($this->targetDir)) {
+            mkdir($this->targetDir);
+        }
+    }
+
+    public function compile(string $configType)
+    {
+        // Set remaining environment that needs reader
+        putenv('APP_ENV='.$configType);
+        $reader = new Reader($this->environment);
+        $config = $reader->get('compile.'.$configType);
+        $basePath = $config['basePath'] ?? '';
+        putenv('APP_BASEPATH='.$basePath);
+
+        // Remove all index.html files
+        $this->deleteContent($this->targetDir);
+
+        // Create pages and parse assets after each created page
+        $slugs    = $reader->getSlugs();
+        $allPages = array_keys($slugs->getAll());
+        $assets = [];
+        foreach ($allPages as $page) {
+            $requestUri = $basePath.'/'.$page;
+            $html = $this->getResponse($config['https'] ?? true, $config['domain'], $requestUri);
+            $this->writeFile($this->targetDir, $page.'/index.html', $html);
+            $assets = array_merge($assets, AssetParser::parse($html));
+        }
+        // Get list of unique assets
+        $assets = array_values(array_filter(array_unique($assets)));
+        $notDeleted = $this->deleteAssets($this->targetDir, $assets);
+        $assets = array_diff($assets, $notDeleted);
+        
+        // Create assets
+        foreach ($assets as $asset) {
+            $requestUri = $basePath.$asset;
+            $source = $this->getResponse($config['https'] ?? true, $config['domain'], $requestUri);
+            $this->writeFile($this->targetDir, $asset, $source);
+        }
+
+        // Sitemap
+        $requestUri = $basePath.'/sitemap.xml';
+        $sitemap = $this->getResponse($config['https'] ?? true, $config['domain'], $requestUri);
+        $this->writeFile($this->targetDir, '/sitemap.xml', $sitemap);
+
+        // Robots
+        $requestUri = $basePath.'/robots.txt';
+        $robots = $this->getResponse($config['https'] ?? true, $config['domain'], $requestUri);
+        $this->writeFile($this->targetDir, '/robots.txt', $robots);
+    }
+
+    private function getResponse(bool $https, string $domain, string $requestUri): string
+    {
+        ob_start();
+        $_SERVER['REQUEST_URI']    = $requestUri;
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['SCRIPT_NAME']    = '/index.php';
+        if ($https) {
+            $_SERVER['SERVER_PORT'] = 443;
+            $_SERVER['HTTPS']       = 'on';
+        } else {
+            $_SERVER['SERVER_PORT'] = 80;
+            unset($_SERVER['HTTPS']);
+        }
+        $_SERVER['HTTP_HOST']       = $domain;
+        $_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36';
+        include $this->environment->getVendorDir().'/flipsite/flipsite/src/index.php';
+        $html = ob_get_contents();
+        ob_end_clean();
+        return $html;
+    }
+
+    private function writeFile(string $targetDir, string $page, string $html): string
+    {
+        $filename = $targetDir.'/'.$page;
+        $filename = str_replace('//', '/', $filename);
+        if (!file_exists(dirname($filename))) {
+            mkdir(dirname($filename), 0777, true);
+        }
+        if (!is_dir($filename)) {
+            file_put_contents($filename, $html);
+        }
+        return $filename;
+    }
+
+    private function deleteContent(string $targetDir): void
+    {
+        $filesystem = new Filesystem();
+        $files      = $this->getDirContents($targetDir);
+        foreach ($files as $file) {
+            if (false !== mb_strpos($file, 'index.html')) {
+                $filesystem->remove($file);
+            }
+        }
+    }
+
+    private function deleteAssets(string $targetDir, array $assets): array
+    {
+        $notDeleted = [];
+        $filesystem = new Filesystem();
+        $images     = $this->getDirContents($targetDir.'/img');
+        $dirs       = [];
+        foreach ($images as $image) {
+            if (is_dir($image)) {
+                $dirs[] = $image;
+                continue;
+            }
+            $asset = str_replace($targetDir, '', $image);
+            if (in_array($asset, $assets)) {
+                $notDeleted[] = $asset;
+            } else {
+                $filesystem->remove($image);
+            }
+        }
+        $videos = $this->getDirContents($targetDir.'/videos');
+
+        foreach ($videos as $video) {
+            if (is_dir($video)) {
+                $dirs[] = $video;
+                continue;
+            }
+            $asset = str_replace($targetDir, '', $video);
+            if (in_array($asset, $assets)) {
+                $notDeleted[] = $asset;
+            } else {
+                $filesystem->remove($video);
+            }
+        }
+        foreach ($dirs as $dir) {
+            if (count(scandir($dir)) == 2) {
+                $filesystem->remove($dir);
+            }
+        }
+        return array_values(array_filter($notDeleted));
+    }
+
+    private function getDirContents(string $dir, &$results = []) : array
+    {
+        if (!is_dir($dir)) {
+            return [];
+        }
+        $files = scandir($dir);
+        foreach ($files as $key => $value) {
+            $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+            if (!is_dir($path)) {
+                $results[] = $path;
+            } elseif ('.' != $value && '..' != $value) {
+                $this->getDirContents($path, $results);
+                $results[] = $path;
+            }
+        }
+        return $results;
+    }
+}
