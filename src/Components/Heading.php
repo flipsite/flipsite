56f938566a9cb1642f88a7012ed863cc7f2c9b27
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2023-10-24 23:52:35 +0300

    Hightlight support

--- src/Components/Heading.php
+++ src/Components/Heading.php
@@ -1,27 +1,31 @@
 <?php
 
 declare(strict_types=1);
+
 namespace Flipsite\Components;
 
 final class Heading extends AbstractComponent
 {
-    use Traits\MarkdownTrait;
+    use Traits\BuilderTrait;
     use Traits\GlobalVarsTrait;
     protected bool $oneline = true;
     protected string $tag   = 'h2';
 
     public function build(array $data, array $style, array $options): void
     {
-        $markdown  = $this->getMarkdownLine((string)$data['value'], $style['markdown'] ?? [], $options['appearance']);
-        $markdown  = $this->checkGlobalVars($markdown);
+        $data['value']  = $this->checkGlobalVars($data['value']);
+        if (isset($style['highlight'])) {
+
+            $data = $this->handleHighlight($data, $style['highlight'], $options['appearance']);
+        }
         $this->addStyle($style);
         if (isset($data['name'])) {
             $a = new Element('a');
-            $a->setContent($markdown);
+            $a->setContent($data['value']);
             $a->setAttribute('name', $data['name']);
             $this->addChild($a);
         } else {
-            $this->setContent($markdown);
+            $this->setContent($data['value']);
         }
     }
 
@@ -30,6 +34,24 @@ final class Heading extends AbstractComponent
         if (!is_array($data)) {
             return ['value' => (string)$data];
         }
+        return $data;
+    }
+
+    private function handleHighlight(array $data, array $style, string $appearance): array
+    {
+        $pattern = '/\[([^\[\]]+)\]/';
+        preg_match_all($pattern, $data['value'], $matches);
+
+        foreach ($matches[1] as $highlightString) {
+            $match = '[us]';
+            $highlightData = $data['highlight'] ?? [];
+            $highlightData['value'] = $highlightString;
+            $style['tag'] = 'span';
+            $span = $this->builder->build('div', $highlightData, $style, ['appearance' => $appearance]);
+            $span->oneline = true;
+            $data['value'] = str_replace('['.$highlightString.']', trim($span->render()), $data['value']);
+        }
+        unset($data['highlight']);
         return $data;
     }
 }
