ec89d5edba3fec278726ffe08ab53dab55441b07
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2023-10-31 14:52:40 +0200

    Form fixes

--- src/Components/Input.php
+++ src/Components/Input.php
@@ -5,21 +5,12 @@ namespace Flipsite\Components;
 
 final class Input extends AbstractComponent
 {
-    use Traits\BuilderTrait;
     protected bool $oneline = true;
     protected bool $empty   = true;
     protected string $tag   = 'input';
 
-    public function build(array $data, array $style, arrat $options): void
+    public function build(array $data, array $style, array $options) : void
     {
         $this->addStyle($style);
-        $this->setAttribute('type', $data['type'] ?? 'text');
-        unset($data['type'], $data['flags']);
-        if (!isset($data['id']) && isset($data['name'])) {
-            $this->setAttribute('id', $data['name']);
-        }
-        foreach ($data as $attribute => $value) {
-            $this->setAttribute($attribute, $value);
-        }
     }
 }
--- src/Components/Textarea.php
+++ src/Components/Textarea.php
@@ -5,23 +5,12 @@ namespace Flipsite\Components;
 
 final class Textarea extends AbstractComponent
 {
-    use Traits\BuilderTrait;
     protected bool $oneline = true;
     protected string $tag   = 'textarea';
 
-    public function build(array $data, array $style, string $appearance): void
+    public function build(array $data, array $style, array $options) : void
     {
         $this->addStyle($style);
-        unset($data['flags']);
-        if (isset($data['value'])) {
-            $this->setContent($data['value']);
-            unset($data['value']);
-        }
-        if (!isset($data['id']) && isset($data['name'])) {
-            $this->setAttribute('id', $data['name']);
-        }
-        foreach ($data as $attribute => $value) {
-            $this->setAttribute($attribute, $value);
-        }
+        $this->setContent($data['value']);
     }
 }
