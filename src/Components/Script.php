d5a13daef3e440853bca45680fd1d294be8eb271
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2023-11-07 10:48:21 +0200

    Script src/inline

--- src/Components/Script.php
+++ src/Components/Script.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 namespace Flipsite\Components;
 
 final class Script extends AbstractComponent
@@ -8,12 +9,22 @@ final class Script extends AbstractComponent
     protected string $tag   = 'script';
     protected bool $oneline = true;
 
-    public function normalize(string|int|bool|array $data) : array
+    public function normalize(string|int|bool|array $data): array
     {
+        if (!is_array($data)) {
+            $data = ['value' => $data];
+        }
+        if (isset($data['value'])) {
+            unset($data['_attr']['src']);
+            unset($data['_attr']['defer']);
+        }
         return $data;
     }
 
-    public function build(array $data, array $style, array $options) : void
+    public function build(array $data, array $style, array $options): void
     {
+        if (isset($data['value'])) {
+            $this->setContent($data['value']);
+        }
     }
 }
