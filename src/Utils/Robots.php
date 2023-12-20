b1289affd6c3c04c84379c1cb7322dbe26a3d582
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2023-12-20 12:59:18 +0200

    Fixes

--- src/Utils/Robots.php
+++ src/Utils/Robots.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 namespace Flipsite\Utils;
 
 use Flipsite\EnvironmentInterface;
@@ -18,14 +17,14 @@ final class Robots
             $rows = [
                 'User-agent: *',
                 'Allow: /',
-                'Sitemap: '.$this->environment->getAbsoluteUrl('sitemap.xml'),
+                'Sitemap: '.trim($this->environment->getAbsoluteUrl('sitemap.xml'), '/'),
             ];
         } else {
             $rows = [
                 'User-agent: *',
                 'Disallow: /',
                 '# No sitemap if not live',
-                '# Sitemap: '.$this->environment->getAbsoluteUrl('sitemap.xml')
+                '# Sitemap: '.trim($this->environment->getAbsoluteUrl('sitemap.xml'), '/')
             ];
         }
         return implode("\n", $rows);
