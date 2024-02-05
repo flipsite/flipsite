cf427ac5537eb9e226c506e463b2f2bda36a0ecd
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-02-05 22:45:10 +0200

    Number stuff

--- src/Components/Number.php
+++ src/Components/Number.php
@@ -5,14 +5,55 @@ namespace Flipsite\Components;
 
 final class Number extends AbstractComponent
 {
+    use Traits\MarkdownTrait;
+    use Traits\ClassesTrait;
+
     protected string $tag  = 'div';
 
     public function build(array $data, array $style, array $options) : void
     {
-        $value = $this->numberToFormat(intval($data['value'] ?? 1), $data['format'] ?? '1');
+        $value = $data['value'] ?? 1;
+        // If , as decimal separator, replace with .
+        $value = str_replace(',','.',$value);
+        $value = str_replace(' ','',$value);
+        $value = floatval($value);
+
+        // number_format(
+        //     float $num,
+        //     int $decimals = 0,
+        //     ?string $decimal_separator = ".",
+        //     ?string $thousands_separator = ","
+        // ): string
+
+        if (isset($data['format'])) {
+            $value = $this->numberToFormat(intval($value), $data['format'] ?? '1');
+        } else {
+            $separators = [
+                'none' => '',
+                'space'=> ' ',
+                'comma'=> ',',
+                'period'=> '.',
+                'apostrophe'=> '\'',
+            ];
+            $decimals = intval($data['decimals'] ?? 0);
+            $decimalSeparator = $data['decimalSeparator'] ?? 'period';
+            $thousandsSeparator = $data['thousandsSeparator'] ?? 'none';
+            $initial = $value;
+            $value = number_format($value, $decimals, $separators[$decimalSeparator], $separators[$thousandsSeparator]);
+            if (floor($initial) != $initial && ($data['removeZeros'] ?? false)) {
+
+                while (substr($value, -1) === '0' || substr($value, -1) === $separators[$decimalSeparator]) {
+                    // Remove the last character from the string
+                    $value = substr($value, 0, -1);
+                }
+            }
+        }
         $this->addStyle($style);
         if (isset($data['content'])) {
-            $this->setContent(str_replace('[number]', $value, $data['content']));
+            $content = str_replace('[number]', (string)$value, $data['content']);
+            $content = $this->getMarkdownLine($content, [], $options['appearance']);
+            $content = $this->addClassesToHtml($content, ['strong'], $style, $options['appearance']);
+            $this->setContent($content);
         } else {
             $this->setContent($value);
         }
@@ -22,6 +63,9 @@ final class Number extends AbstractComponent
     {
         if ($number < 1 || $number > 26) {
             return (string)$number;
+        }
+        if ($format === '1.') {
+            return $number.'.';
         }
 
         $lowercaseLetters = range('a', 'z');
@@ -38,7 +82,6 @@ final class Number extends AbstractComponent
             case 'I':
                 return strtoupper($romanNumerals[$number - 1]);
         }
-
         return (string)$number;
     }
 }
