beb71ec1838e94da59cf607b876e84ea4fa7b5d4
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-05-03 21:01:52 +0300

    Date fix

--- src/Components/Date.php
+++ src/Components/Date.php
@@ -1,22 +1,20 @@
 <?php
 
 declare(strict_types=1);
+
 namespace Flipsite\Components;
 
 final class Date extends AbstractComponent
 {
     protected string $tag  = 'time';
 
-    public function build(array $data, array $style, array $options) : void
+    public function build(array $data, array $style, array $options): void
     {
         $this->addStyle($style);
-        $timestamp = strtotime($data['value']);
-        if (!$timestamp) {
-            $timestamp = time();
-        }
-        $date = date($data['format'] ?? 'Y-m-d', $timestamp );
+        $timestamp = isset($data['value']) ? strtotime($data['value']) : time();
+        $date = date($data['format'] ?? 'Y-m-d', $timestamp);
         if (isset($data['content'])) {
-            $this->setContent(str_replace('[date]',$date,$data['content']));
+            $this->setContent(str_replace('[date]', $date, $data['content']));
         } else {
             $this->setContent($date);
         }
