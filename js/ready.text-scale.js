2947874817489e9e1735561e041364506d839ea6
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-05-06 16:25:54 +0300

    WIP

--- js/ready.text-scale.js
+++ js/ready.text-scale.js
@@ -1,14 +1,25 @@
 ready(() => {
   const elements = document.querySelectorAll('[data-text-scale]');
   if (elements.length === 0) return;
+  const getTextWidth = function(element) {
+    const offscreen = element.cloneNode(true)
+    offscreen.style.transform = 'scale(1)';
+    offscreen.style.width = 'auto';
+    offscreen.style.height = 'auto';
+    offscreen.style.display = 'inline-block';
+    document.body.appendChild(offscreen);
+    const width = offscreen.offsetWidth;
+    document.body.removeChild(offscreen);
+    return width;
+  }
   const updateScale = () => {
     elements.forEach(element => {
       element.style.transform = 'scale(1)';
-      const rect = element.getBoundingClientRect();
       const parentRect = element.parentNode.getBoundingClientRect();
       const parentStyle = window.getComputedStyle(element.parentNode);
       const maxWidth = parentRect.width - parseFloat(parentStyle.paddingLeft) - parseFloat(parentStyle.paddingRight);
-      const scale = maxWidth / rect.width;
+      const textWidth = getTextWidth(element);
+      const scale = maxWidth / textWidth;
       if (scale < 1) {
         element.style.transform = 'scale('+scale+')';
       }
