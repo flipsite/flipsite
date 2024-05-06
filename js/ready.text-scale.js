1994f432ed209333d2f4be8701bf035c57868aaa
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-05-06 19:15:12 +0300

    Improved text scale

--- js/ready.text-scale.js
+++ js/ready.text-scale.js
@@ -1,33 +1,94 @@
 ready(() => {
   const elements = document.querySelectorAll('[data-text-scale]');
+  elements.forEach(element => {
+    const initialFontSize = parseFloat(window.getComputedStyle(element).fontSize);
+    element.setAttribute('data-initial-font-size', initialFontSize);
+  });
   if (elements.length === 0) return;
-  const getTextWidth = function(element) {
+  const getFontSize = function(element, maxWidth) {
     const offscreen = element.cloneNode(true)
-    offscreen.style.transform = 'scale(1)';
     offscreen.style.width = 'auto';
     offscreen.style.height = 'auto';
     offscreen.style.display = 'inline-block';
     document.body.appendChild(offscreen);
-    const width = offscreen.offsetWidth;
-    document.body.removeChild(offscreen);
-    return width;
+    let fontSize = element.getAttribute('data-initial-font-size');
+    offscreen.style.fontSize = fontSize + 'px';
+    if (offscreen.offsetWidth <= maxWidth) {
+      offscreen.remove();
+      return fontSize + 'px';
+    }
+    const letterCount = element.innerText.length;
+    let i = 0;
+    while (i < 100 && offscreen.offsetWidth > maxWidth) {
+      const factor = (offscreen.offsetWidth - maxWidth) / letterCount
+      fontSize -= factor
+      offscreen.style.fontSize = fontSize + 'px';
+      i++;
+    }
+    offscreen.remove();
+    return fontSize + 'px';
   }
   const updateScale = () => {
+    console.log('updateScale')
     elements.forEach(element => {
-      element.style.transform = 'scale(1)';
       const parentRect = element.parentNode.getBoundingClientRect();
       const parentStyle = window.getComputedStyle(element.parentNode);
       const maxWidth = parentRect.width - parseFloat(parentStyle.paddingLeft) - parseFloat(parentStyle.paddingRight);
-      const textWidth = getTextWidth(element);
-      const scale = maxWidth / textWidth;
-      if (scale < 1) {
-        element.style.transform = 'scale('+scale+')';
-      }
+      element.style.fontSize = getFontSize(element, maxWidth)
     });
   }
-  window.addEventListener('resize', updateScale );
+
+  const resizeObserver = new ResizeObserver(updateScale);
+  elements.forEach(element => {
+    resizeObserver.observe(element.parentNode);
+  });
+
   document.fonts.ready.then(() => {
     updateScale();
   });
   updateScale();
 });
+// ready(() => {
+//   const elements = document.querySelectorAll('[data-text-scale]');
+//   elements.forEach(element => {
+//     const initialFontSize = parseFloat(window.getComputedStyle(element).fontSize);
+//     element.setAttribute('data-initial-font-size', initialFontSize);
+//   });
+//   if (elements.length === 0) return;
+//   const getFontSize = function(element,maxWidth) {
+//     const offscreen = element.cloneNode(true)
+//     offscreen.style.width = 'auto';
+//     offscreen.style.height = 'auto';
+//     offscreen.style.display = 'inline-block';
+//     document.body.appendChild(offscreen);
+//     let fontSize = element.getAttribute('data-initial-font-size');
+//     offscreen.style.fontSize = fontSize+'px';
+//     if (offscreen.offsetWidth <= maxWidth) {
+//       offscreen.remove();
+//       return fontSize+'px';
+//     }
+//     const letterCount = element.innerText.length;
+//     let i = 0;
+//     while (i < 100 && offscreen.offsetWidth > maxWidth)  {
+//       const factor =  (offscreen.offsetWidth-maxWidth)/letterCount
+//       fontSize-=factor
+//       offscreen.style.fontSize = fontSize+'px';
+//       i++;
+//     }
+//     offscreen.remove();
+//     return fontSize+'px';
+//   }
+//   const updateScale = () => {
+//     elements.forEach(element => {
+//       const parentRect = element.parentNode.getBoundingClientRect();
+//       const parentStyle = window.getComputedStyle(element.parentNode);
+//       const maxWidth = parentRect.width - parseFloat(parentStyle.paddingLeft) - parseFloat(parentStyle.paddingRight);
+//       element.style.fontSize = getFontSize(element,maxWidth)
+//     });
+//   }
+//   window.addEventListener('resize', updateScale );
+//   document.fonts.ready.then(() => {
+//     updateScale();
+//   });
+//   updateScale();
+// });
