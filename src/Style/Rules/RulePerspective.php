f13b7a343cbf27bba7a38bbfcfc35da549379a26
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-08-19 10:29:31 +0300

    Perspective fix

--- src/Style/Rules/RulePerspective.php
+++ src/Style/Rules/RulePerspective.php
@@ -1,20 +1,10 @@
 <?php
 
 declare(strict_types=1);
-
 namespace Flipsite\Style\Rules;
 
-class RulePerspective extends AbstractRule
+class RulePerspective extends AbstractRuleTransform
 {
-    
-/**
-     * @param array<string> $args
-     */
-    protected function process(array $args) : void
-    {
-        $value = $this->checkCallbacks('size', $args);
-        $this->setDeclaration('--tw-perspective', $value);
-        $this->setDeclaration('transform', 'perspective(var(--tw-perspective)) translateX(var(--tw-translate-x)) translateY(var(--tw-translate-y)) rotate(var(--tw-rotate)) rotateX(var(--tw-rotate-x)) rotateY(var(--tw-rotate-y)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));');
-
-    }
+    protected array $properties = ['--tw-perspective'];
+    protected array $callbacks  = ['size'];
 }
