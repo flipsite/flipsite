be0c333ce12d643d1ed483c3fbd386662f0157dd
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-09-18 11:31:56 +0300

    Check orginal

--- src/Assets/Sources/AssetSourcesInterface.php
+++ src/Assets/Sources/AssetSourcesInterface.php
@@ -2,19 +2,30 @@
 
 declare(strict_types=1);
 namespace Flipsite\Assets\Sources;
+
 use Psr\Http\Message\ResponseInterface as Response;
 
 interface AssetSourcesInterface
 {
     public function getList() : array;
+
     public function getInfo(string $asset) : ?AbstractAssetInfo;
+
     public function addBasePath(AssetType $type, string $asset) : string;
+
     public function isOrginal(string $asset) : bool;
+
     public function isCached(string $asset) : bool;
+
     public function addToCache(string $asset, string $encoded) : bool;
-    public function getResponse(Response $response, string $asset) : Response;
+
+    public function getResponse(Response $response, string $asset, bool $checkCache = true): Response;
+
     public function getCached(string $asset) : string;
+
     public function upload(AssetType $type, string $filename, string $filepath) : bool|string;
+
     public function rename(AssetType $type, string $filename, string $newFilename) : bool|string;
+
     public function delete(AssetType $type, string $filename) : bool;
 }
