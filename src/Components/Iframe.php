1a8304ef255d091b2173f01acc0ee313264145fd
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-10-07 12:42:11 +0300

    Iframe sandbox

--- src/Components/Iframe.php
+++ src/Components/Iframe.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 namespace Flipsite\Components;
 
+use Flipsite\Utils\ArrayHelper;
+
 class Iframe extends AbstractComponent
 {
     protected bool $oneline = true;
@@ -10,6 +12,18 @@ class Iframe extends AbstractComponent
 
     public function build(array $data, array $style, array $options): void
     {
+        $width = intval($data['width'] ?? 0);
+        if ($width) {
+            $this->setAttribute('width', $width);
+        }
+        $height = intval($data['height'] ?? 0);
+        if ($height) {
+            $this->setAttribute('height', $height);
+        }
+        if (isset($data['sandbox'])) {
+            $list = ArrayHelper::decodeJsonOrCsv($data['sandbox']);
+            $this->setAttribute('sandbox', implode(' ', $list));
+        }
         $this->addStyle($style);
     }
 }
