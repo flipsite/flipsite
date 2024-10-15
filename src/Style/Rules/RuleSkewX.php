ba17a3bf783ed5f70638221ad187e16fdd1e6527
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-10-15 10:10:19 +0300

    Skew fix

--- src/Style/Rules/RuleSkewX.php
+++ src/Style/Rules/RuleSkewX.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 namespace Flipsite\Style\Rules;
 
-final class RuleSkewX extends AbstractRule
+final class RuleSkewX extends AbstractRuleTransform
 {
     protected array $properties = ['--tw-skew-x'];
     protected string $unit      = 'deg';
--- src/Style/Rules/RuleSkewY.php
+++ src/Style/Rules/RuleSkewY.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 namespace Flipsite\Style\Rules;
 
-final class RuleSkewY extends AbstractRule
+final class RuleSkewY extends AbstractRuleTransform
 {
     protected array $properties = ['--tw-skew-y'];
     protected string $unit      = 'deg';
