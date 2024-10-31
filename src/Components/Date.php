0544a1a292b46a01857643d640f21c2b6f58a11f
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-10-31 20:27:09 +0200

    IntlDateFormatter

--- src/Components/Date.php
+++ src/Components/Date.php
@@ -6,13 +6,36 @@ namespace Flipsite\Components;
 
 final class Date extends AbstractComponent
 {
+    use Traits\PathTrait;
     protected string $tag  = 'time';
 
     public function build(array $data, array $style, array $options): void
     {
         $this->addStyle($style);
         $timestamp = isset($data['value']) ? strtotime($data['value']) : time();
-        $date = date($data['format'] ?? 'Y-m-d', $timestamp);
+        $format = \IntlDateFormatter::FULL;
+        switch ($data['format'] ?? '') {
+            case 'full': $format = \IntlDateFormatter::FULL;
+                break;
+            case 'long': $format = \IntlDateFormatter::LONG;
+                break;
+            case 'medium': $format = \IntlDateFormatter::MEDIUM;
+                break;
+            case 'short': $format = \IntlDateFormatter::SHORT;
+                break;
+            case 'none': $format = \IntlDateFormatter::NONE;
+                break;
+
+        }
+        $dateFormatter = new \IntlDateFormatter(
+            (string)$this->path->getLanguage(),
+            $format,    // Date format (can be SHORT, MEDIUM, LONG, FULL)
+            \IntlDateFormatter::NONE,     // Time format (NONE means no time, or you can set it)
+            null,
+            null,
+            $data['pattern'] ?? null
+        );
+        $date = $dateFormatter->format($timestamp);
         if (isset($data['content'])) {
             $this->setContent(str_replace('[date]', $date, $data['content']));
         } else {
