b21dcf0800827499b6c41b42ed0bd0f611bf947d
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2024-11-09 18:55:51 +0200

    Tags fix

--- src/Components/Traits/MarkdownTrait.php
+++ src/Components/Traits/MarkdownTrait.php
@@ -1,15 +1,16 @@
 <?php
 
 declare(strict_types=1);
+
 namespace Flipsite\Components\Traits;
 
 trait MarkdownTrait
 {
     use ActionTrait;
 
-    private function getMarkdownLine(string $text, array $style, string $appearance) : string
+    private function getMarkdownLine(string $text, array $style, string $appearance): string
     {
-        $text      = strip_tags($text);
+        $text      = strip_tags($text, '<br><span>');
         $parsedown = new \Parsedown();
         $text      = str_replace("\n", ' ', $text);
         $text      = trim($text);
