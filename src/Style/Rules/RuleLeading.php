53d95eb88ac3dd1ceaa7fe85594882879cbc9775
Henrik Karlstrom <henrik.karlstrom@gmail.com>
2025-12-08 21:15:54 +0200

    Line height to rules.yaml

--- src/Style/Rules/RuleLeading.php
+++ src/Style/Rules/RuleLeading.php
@@ -9,9 +9,8 @@ final class RuleLeading extends AbstractRule
     /**
      * @param array<string> $args
      */
-    protected function process(array $args) : void
+    protected function process(array $args): void
     {
-        $value ??= $this->getConfig('lineHeight', $args[0]);
         $value ??= $this->checkCallbacks('size', $args);
         $this->setDeclaration('line-height', $value);
     }
